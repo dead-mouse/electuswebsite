f3147057c888f2473ae4628617845ea379225e5d
dead-mouse <dinnu8055@gmail.com>
2019-01-08 15:30:02 +0530

    day2injc

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,5 +1,5 @@
 alert("welcome to the calculator");
-let num1=prompt("enter nbr1");
+let num1= prompt(Number("enter nbr1"));
 let num2=prompt("enter nbr2");
 let nbr3=prompt("enter nbr3");
 console.log(`addition::
--- javascript/typeconversion.js
+++ javascript/typeconversion.js
@@ -12,3 +12,10 @@ console.log(typeof data1);//number
 let converteddata1 =Number(data1);// we need to add word accordingly
 console.log(converteddata1);//12
 console.log(typeof converteddata1);//string
+
+let data2 =true;
+console.log(data2);//12
+console.log(typeof data2);//boolean
+let converteddata2 =Number(data2);// we need to add word accordingly
+console.log(converteddata2);//12
+console.log(typeof converteddata2);//string
